f036b38e2dd61f2933e1460cc3cde3750d8c479a
Jack South <jacksouth@MacBook-Air.lan>
2020-06-08 22:28:16 -0400

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Endangered from './Endangered.js';
-import Header from './Header.js'
+import Endangered from './Endangered.jsx';
+import Header from './Header.jsx'
 import Footer from './Footer.js'
 import './App.css'
 
